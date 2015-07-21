0e432658868c03f2f844437aebfbefca8f97cc6e
jlevinger <joe@joelevinger.com>
2015-07-21 14:33:38 -0400

    Answers to quiz

--- quiz.sql
+++ quiz.sql
@@ -8,11 +8,17 @@ CREATE DATABASE westeros;
 
 -- your code starts here
 
+CREATE TABLE houses (id SERIAL PRIMARY KEY, name TEXT, motto TEXT, sigil TEXT);
+CREATE TABLE people (id SERIAL PRIMARY KEY, name TEXT, title TEXT, quote TEXT, image TEXT, age INTEGER);
+
 -- your code ends here
 
 -- Question 2. Load the data from `westeros/houses.csv` into the `houses` table and the data from `westeros/people.csv` into the `people` table.
 
 -- your code starts here
+
+\copy houses(name, motto, sigil) FROM './westeros/houses.csv' WITH (FORMAT csv, HEADER true)
+\copy people(name, title, quote, image, age) FROM './westeros/people.csv' WITH (FORMAT csv, HEADER true)
 
 -- your code ends here
 
@@ -22,11 +28,19 @@ CREATE DATABASE westeros;
 
 -- your code starts here
 
+INSERT INTO people(name, title, quote, image, age) VALUES('Cersei Baratheon', 'King Mother and Lady of Casterly Rock', '"The gods have no mercy, that''s why they’re gods."', 'http://i.imgur.com/jYeeCJ3.jpg', 40);
+INSERT INTO houses(name, motto, sigil) VALUES('House Baratheon', 'Ours is the fury.', 'http://i.imgur.com/iJtBKIR.png');
+
 -- your code ends here
 
 -- Question 4. Write a query to get all attributes of people greater than a certain age *or* having a specific title.
 
 -- your code starts here
+
+SELECT *
+FROM people
+WHERE age > 40
+  OR title = 'Lord Commander of the Night''s Watch';
 
 -- your code ends here
 
@@ -34,10 +48,17 @@ CREATE DATABASE westeros;
 
 -- your code starts here
 
+UPDATE houses
+SET motto = 'Our blades are dull, not sharp!'
+WHERE name = 'House Bolton';
+
 -- your code ends here
 
 -- Question 6. Kill off, er, I mean delete one of the people. Look them up by their title.
 
 -- your code starts here
+
+DELETE FROM people
+WHERE title = 'Hand of the King and Master of Coin';
 
 -- your code ends here
