58f4608bc5801027761acf5c44b92d18a6e20500
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-21 14:25:48 -0400

    Assessment Completed. Tests match objectives.

--- quiz.sql
+++ quiz.sql
@@ -6,38 +6,43 @@ CREATE DATABASE westeros;
 --   `houses` has the following columns: id (serial primary key), name (text), motto (text), and sigil (text)
 --   `people` has an id ( serial primary key), name (text), title (text), quote (text), image (text), and age (integer)
 
--- your code starts here
+CREATE TABLE houses (id SERIAL PRIMARY KEY, name TEXT, motto TEXT, sigil TEXT);
 
--- your code ends here
+CREATE TABLE people (id SERIAL PRIMARY KEY, name TEXT, title TEXT, quote TEXT, image TEXT, age INTEGER);
 
 -- Question 2. Load the data from `westeros/houses.csv` into the `houses` table and the data from `westeros/people.csv` into the `people` table.
 
--- your code starts here
+\copy houses(name, motto, sigil) FROM './westeros/houses.csv' WITH (FORMAT csv, HEADER true)
 
--- your code ends here
+\copy people(name, title, quote, image, age) FROM './westeros/people.csv' WITH (FORMAT csv, HEADER true)
 
 -- Question 3. Insert `Cersei Baratheon,King Mother and Lady of Casterly Rock,"The gods have no mercy, that’s why they’re gods.",http://i.imgur.com/jYeeCJ3.jpg,40`
 -- into the people table, and `House Baratheon,Ours is the fury.,http://i.imgur.com/iJtBKIR.png`
 -- into the houses table
 
--- your code starts here
+INSERT INTO people(name, title, quote, image, age) VALUES('Cersei Baratheon', 'King Mother and Lady of Casterly Rock', 'The gods have no mercy, that’s why they’re gods.', 'http://i.imgur.com/jYeeCJ3.jpg', 40);
 
--- your code ends here
+INSERT INTO houses(name, motto, sigil) VALUES('House Baratheon', 'Ours is the fury.', 'http://i.imgur.com/iJtBKIR.png');
 
 -- Question 4. Write a query to get all attributes of people greater than a certain age *or* having a specific title.
 
--- your code starts here
+SELECT *
+FROM people
+WHERE age > 20
+ORDER BY age;
 
--- your code ends here
+SELECT *
+FROM people
+WHERE name = 'Cersei Baratheon'
+ORDER BY name;
 
 -- Question 5. Change the motto of one of your houses. Look it up by its name.
 
--- your code starts here
-
--- your code ends here
+UPDATE houses
+SET motto = 'Work Hard, Play Hard.'
+WHERE name = 'House Bolton';
 
 -- Question 6. Kill off, er, I mean delete one of the people. Look them up by their title.
 
--- your code starts here
-
--- your code ends here
+DELETE FROM people
+  WHERE title = 'Queen of Meereen and Mother of Dragons';
