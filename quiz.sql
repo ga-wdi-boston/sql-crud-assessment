ba8af785081cd875889eb8f52aed75f0f51b3dbc
Maksim <dollydasheep@gmail.com>
2015-07-21 14:37:02 -0400

    Completed assessment

--- quiz.sql
+++ quiz.sql
@@ -7,13 +7,15 @@ CREATE DATABASE westeros;
 --   `people` has an id ( serial primary key), name (text), title (text), quote (text), image (text), and age (integer)
 
 -- your code starts here
-
+CREATE TABLE houses (id SERIAL PRIMARY KEY, name TEXT, motto TEXT, sigil TEXT);
+CREATE TABLE people (id SERIAL PRIMARY KEY, name TEXT, title TEXT, quote TEXT, image TEXT, age INT);
 -- your code ends here
 
 -- Question 2. Load the data from `westeros/houses.csv` into the `houses` table and the data from `westeros/people.csv` into the `people` table.
 
 -- your code starts here
-
+\copy houses(name, motto, sigil) FROM './westeros/houses.csv' WITH (FORMAT csv, HEADER true)
+\copy people(name, title, quote, image, age) FROM './westeros/people.csv' WITH (FORMAT csv, HEADER true);
 -- your code ends here
 
 -- Question 3. Insert `Cersei Baratheon,King Mother and Lady of Casterly Rock,"The gods have no mercy, that’s why they’re gods.",http://i.imgur.com/jYeeCJ3.jpg,40`
@@ -21,23 +23,24 @@ CREATE DATABASE westeros;
 -- into the houses table
 
 -- your code starts here
-
+INSERT INTO people(name, title, quote, image, age) VALUES('Cersei Baratheon', 'King Mother and Lady of Casterly Rock', 'The gods have no mercy, that’s why they’re gods.', 'http://i.imgur.com/jYeeCJ3.jpg', 40);
+INSERT INTO houses(name, motto, sigil) VALUES('House Baratheon', 'Ours is the fury.', 'http://i.imgur.com/iJtBKIR.png');
 -- your code ends here
 
 -- Question 4. Write a query to get all attributes of people greater than a certain age *or* having a specific title.
 
 -- your code starts here
-
+SELECT * FROM PEOPLE WHERE age > 30 OR title = 'Princess of Winterfell';
 -- your code ends here
 
 -- Question 5. Change the motto of one of your houses. Look it up by its name.
 
 -- your code starts here
-
+UPDATE houses SET motto = 'Hear me bark!' WHERE name = 'House Lannister';
 -- your code ends here
 
 -- Question 6. Kill off, er, I mean delete one of the people. Look them up by their title.
 
 -- your code starts here
-
+DELETE FROM people WHERE title = 'Hand of the King and Master of Coin';
 -- your code ends here
