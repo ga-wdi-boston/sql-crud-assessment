504c3158304e1cbdb18fe2f18c912c34d8eefb7c
Antony Donovan <antony.donovan@generalassemb.ly>
2015-10-26 22:10:22 -0400

    Turn off pager in script
    This prevents lost output

--- assessment.sql
+++ assessment.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS "sql-crud-assessment";
 CREATE DATABASE "sql-crud-assessment";
+\pset pager 0
 \c "sql-crud-assessment"
 
 -- Question 1. Create a table called `licensees` to hold data in columns surname,given_name,gender,height,weight,dob,eye_color,hair_color.  See `data/licensees.csv` for example data.  Don't forget that all tables should have an id column as a unique identifier for a row.
