35ae33e71265a6088a8dac4150055471b6c1ef86
BMorse09 <benmorse09@gmail.com>
2016-09-22 09:23:06 -0400

    Supplied answers for all questions

--- diagnostic.sql
+++ diagnostic.sql
@@ -3,26 +3,42 @@ CREATE DATABASE "sql-crud-assessment";
 \pset pager 0
 \c "sql-crud-assessment"
 
--- Question 1. Create a table called `licensees` to hold data in columns surname, given_name, gender, height, weight, born_on, eye_color, hair_color.  See `data/licensees.csv` for example data.  Don't forget that all tables should have an id column as a unique identifier for a row.
+-- Question 1. Create a table called `licensees` to hold data in columns surname,
+-- given_name, gender, height, weight, born_on, eye_color, hair_color.
+-- See `data/licensees.csv` for example data.  Don't forget that all tables should have an id column as a unique identifier for a row.
 
--- your code here
+  CREATE TABLE licenses (
+    id SERIAL PRIMARY KEY
+    surname TEXT,
+    given_name TEXT,
+    gender CHAR(1),
+    height NUMERIC (2,1),
+    weight NUMERIC (3,1),
+    born_on DATE,
+    eye_color CHAR(2),
+    hair_color CHAR(2)
+  );
 
 -- Question 2. Add `Bishop, Caren, f, 63, 132, 1943-09-26, Brown, Black` into the table of licensees.
 
--- your code here
+ INSERT INTO licenses
+  (surname, given_name, gender, height, weight, born_on, eye_color, hair_color) VALUES
+  ('Bishop', 'Caren', 'f', 63, 132, 1943-09-26, 'Brown', 'Black');
 
 -- After completing questions 1 and 2 uncomment the following to load data from `data/licensees.csv` into the `licensees` table.
 
--- \copy licensees(surname,given_name,gender,height,weight,born_on,eye_color,hair_color) FROM './data/licensees.csv' WITH (FORMAT csv, HEADER true)
+\copy licensees(surname,given_name,gender,height,weight,born_on,eye_color,hair_color) FROM './data/licensees.csv' WITH (FORMAT csv, HEADER true)
 
 -- Question 3. Write a query to get all attributes of licensees with Hazel eye color and Brown or Black hair color.
 
--- your code here
+SELECT * FROM licenses WHERE eye_color = 'Hazel' and hair_color in ('Brown', 'Black');
 
 -- Question 4. Make each of Marilynn Escobar and Chris Whaley one inch shorter. Look them up by their names.
 
--- your code here
+UPDATE licenses
+set height = -1
+where (surname = 'Escobar' and given_name = 'Marilynn') and (surname = 'Whaley' and given_name = 'Chris');
 
 -- Question 5. Remove Dylan Rich and Teresita Myers from the table of licensees.
 
--- your code here
+DELETE * FROM licenses WHERE (surname = 'Rich' and given_name = 'Dylan') and (surname = 'Myers' and given_name = 'Teresita');
