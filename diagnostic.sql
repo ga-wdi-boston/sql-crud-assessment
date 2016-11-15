1dee149076fccfa9f635c989790c6891fe4e422c
sphilippe7 <solomon924@gmail.com>
2016-11-15 09:22:02 -0500

    SQL Diagnostic on 11/15/16
    comfort: 3
    clarity: 4
    
    Felt good about this one after reviewing some documentation.
    I feel as though I got all of them right except for 3 and 4. I could
    not get a definitive answer even after reviewing my notes.

--- diagnostic.sql
+++ diagnostic.sql
@@ -5,19 +5,28 @@ CREATE DATABASE "sql-crud-assessment";
 
 -- Question 1. Create a table called `licensees` to hold data in columns surname, given_name, gender, height, weight, born_on, eye_color, hair_color.  See `data/licensees.csv` for example data.  Don't forget that all tables should have an id column as a unique identifier for a row.
 
--- your code here
+CREATE TABLE licensees (
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  height INTEGER,
+  weight INTEGER,
+  born_on DATE
+  );
+
 
 -- Question 2. Add `Bishop, Caren, f, 63, 132, 1943-09-26, Brown, Black` into the table of licensees.
 
--- your code here
+INSERT INTO licensees VALUES ('Bishop', 'Caren', 'f', 63, 132, 1943-09, 'Brown', 'Black');
 
 -- After completing questions 1 and 2 uncomment the following to load data from `data/licensees.csv` into the `licensees` table.
 
--- \copy licensees(surname,given_name,gender,height,weight,born_on,eye_color,hair_color) FROM './data/licensees.csv' WITH (FORMAT csv, HEADER true)
+\copy licensees(surname,given_name,gender,height,weight,born_on,eye_color,hair_color) FROM './data/licensees.csv' WITH (FORMAT csv, HEADER true)
 
 -- Question 3. Write a query to get all attributes of licensees with Hazel eye color and Brown or Black hair color.
 
--- your code here
+
 
 -- Question 4. Make each of Marilynn Escobar and Chris Whaley one inch shorter. Look them up by their names.
 
@@ -25,4 +34,4 @@ CREATE DATABASE "sql-crud-assessment";
 
 -- Question 5. Remove Dylan Rich and Teresita Myers from the table of licensees.
 
--- your code here
+DELETE FROM licensees WHERE given_name = 'Dylan Rich', 'Teresita Myers';
